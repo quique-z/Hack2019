7875ba7e7b00b80735d87783036fb9abcd5b457e
quique-z <manunikus@hotmail.com>
2019-12-05 13:31:52 -0800

    Update get-app.sh

--- get-app.sh
+++ get-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 size=""
 ulimit -n 20000
-/usr/bin/wget -O /opt/server https://github.com/vmssignite/ignite2019/raw/controlcrash/server
+/usr/bin/wget -O /opt/server https://github.com/quique-z/Hack2019/raw/master/server
 sleep 5
 chmod +x /opt/server
 /opt/server
